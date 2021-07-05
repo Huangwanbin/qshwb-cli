101952e0081a78ecde44866d06ec6fa3258c47d6
huangwanbin <wanbin_huang@qingsongchou.com>
2021-07-05 10:08:44 +0800

    add fs-extra && modify create.js

--- bin/create.js
+++ bin/create.js
@@ -1,3 +1,22 @@
-module.exports = function(name,options){
+const path = require('path');
+const fs = require('fs-extra');
+
+module.exports = async function(name,options){
     console.log('name:',name,'options:',options)
+    //获取当前命令行执行地址
+    const cwd = process.cwd();
+    //创建的目录地址
+    const targetAir = path.join(cwd,name);
+    //判断目录是否存在
+    if (fs.existsSync(targetAir)) {
+        if (options.force) {
+            await fs.remove(targetAir)
+        }else{
+            //TODO 询问用户是否覆盖
+            console.log('询问用户是否覆盖');
+        }
+    }else{
+        //TODO 直接创建目录
+        console.log('直接创建');
+    }
 }
